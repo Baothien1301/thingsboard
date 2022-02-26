45890f3426c107abc6a50efa84737cb39a5943f1
BaoBao <pnbaokdd1@gmail.com>
2022-02-26 09:20:39 +0700

    Change background color

--- ui-ngx/src/app/shared/components/json-form/react/styles/thingsboardTheme.ts
+++ ui-ngx/src/app/shared/components/json-form/react/styles/thingsboardTheme.ts
@@ -35,7 +35,7 @@ import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 import { mergeDeep } from '@core/utils';
 
-const PRIMARY_COLOR = '#305680';
+const PRIMARY_COLOR = '#cca526';//'#305680';
 const SECONDARY_COLOR = '#527dad';
 const HUE3_COLOR = '#a7c1de';
 
